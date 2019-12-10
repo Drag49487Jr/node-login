63730b0f96765f55cc2beabea28d3289fe263d76
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-12-10 00:02:19 -0600

    node register functions/returns req.body

--- server.js
+++ server.js
@@ -25,16 +25,16 @@ app.post('/login', (req, res) => {
 
 app.post('/register', async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
     } catch {
-        res.redirect('/register')
+      res.redirect('/register')
     }
     console.log(users)
 })
